68865e3cafca9124fa10b9c06226febb2417c458
Edouard Demotes-Mainard <edouard@lafabriquenouvelle.com>
2018-02-15 13:15:45 -0500

    Adapt tests

--- test/index.js
+++ test/index.js
@@ -10,7 +10,7 @@ const CatboxFake = require('../lib');
 // Test shortcuts
 
 const { describe, it } = exports.lab = Lab.script();
-const expect = Code.expect;
+const { expect, fail } = Code;
 
 describe('Fake', () => {
 
@@ -53,33 +53,47 @@ describe('Fake', () => {
         expect(client.isReady()).to.equal(true);
     });
 
-    it('should not be ready if alwaysNotReady is true', async (done) => {
+    it('should not be ready if ready is false', async (done) => {
 
-        const client = new Catbox.Client(CatboxFake, { alwaysNotReady: true });
+        let trojan = {};
+        const client = new Catbox.Client(CatboxFake, {
+            trojan: (m) => {
+
+                trojan = m;
+            }
+        });
 
         await client.start();
+        trojan.ready(false);
 
         expect(client.isReady()).to.equal(false);
     });
 
-    it('should be ready if alwaysReady is true', async (done) => {
+    it('should be ready if ready is true', async (done) => {
 
-        const client = new Catbox.Client(CatboxFake, { alwaysReady: true });
+        let trojan = {};
+        const client = new Catbox.Client(CatboxFake, {
+            trojan: (m) => {
+
+                trojan = m;
+            }
+        });
 
         await client.start();
         await client.stop();
+        trojan.ready(true);
 
         expect(client.isReady()).to.equal(true);
     });
 
-    it('should fail if alwaysReady and alwaysNotReady are true', (done) => {
+    it('should fail if trojan is not a function', (done) => {
 
         let client;
         try {
-            client = new Catbox.Client(CatboxFake, { alwaysReady: true, alwaysNotReady: true });
+            client = new Catbox.Client(CatboxFake, { trojan: 'nothing' });
         }
         catch (err) {
-            expect(err.message).to.equal('Must set either alwaysReady or alwaysNotReady, not both');
+            expect(err.message).to.equal('Trojan must be a function');
         }
 
         expect(client).to.equal(undefined);
@@ -87,7 +101,7 @@ describe('Fake', () => {
 
     it('should get fake item', async (done) => {
 
-        const client = new Catbox.Client(CatboxFake, { alwaysReady: true });
+        const client = new Catbox.Client(CatboxFake);
 
         await client.start();
 
@@ -104,7 +118,7 @@ describe('Fake', () => {
 
     it('should set fake item', async (done) => {
 
-        const client = new Catbox.Client(CatboxFake, { alwaysReady: true });
+        const client = new Catbox.Client(CatboxFake);
 
         await client.start();
 
@@ -120,7 +134,7 @@ describe('Fake', () => {
 
     it('should drop fake item', async (done) => {
 
-        const client = new Catbox.Client(CatboxFake, { alwaysReady: true });
+        const client = new Catbox.Client(CatboxFake);
 
         await client.start();
 
@@ -130,5 +144,117 @@ describe('Fake', () => {
         };
 
         await client.drop(key);
+    });
+
+    it('should throw an error if throw is enable', async (done) => {
+
+        let trojan = {};
+        const client = new Catbox.Client(CatboxFake, {
+            trojan: (m) => {
+
+                trojan = m;
+            }
+        });
+
+        const key = {
+            id: 'any_key',
+            segment: 'any_segment'
+        };
+        const value = 'value';
+        const _ttl = 9999;
+
+        trojan.throw(true);
+
+        try {
+            await client.start();
+            fail('Should fail before this');
+        }
+        catch (err) {
+            expect(err.message).to.equal('Catbox Fake Error');
+        }
+
+        try {
+            await client.set(key, value, _ttl);
+            fail('Should fail before this');
+        }
+        catch (err) {
+            expect(err.message).to.equal('Catbox Fake Error');
+        }
+
+        try {
+            await client.get(key);
+            fail('Should fail before this');
+        }
+        catch (err) {
+            expect(err.message).to.equal('Catbox Fake Error');
+        }
+
+        try {
+            await client.drop(key);
+            fail('Should fail before this');
+        }
+        catch (err) {
+            expect(err.message).to.equal('Catbox Fake Error');
+        }
+
+        try {
+            client.isReady();
+            fail('Should fail before this');
+        }
+        catch (err) {
+            expect(err.message).to.equal('Catbox Fake Error');
+        }
+
+        try {
+            await client.stop();
+            fail('Should fail before this');
+        }
+        catch (err) {
+            expect(err.message).to.equal('Catbox Fake Error');
+        }
+    });
+
+
+    it('should delay response if a delay is given', async (done) => {
+
+        let trojan = {};
+        const client = new Catbox.Client(CatboxFake, {
+            trojan: (m) => {
+
+                trojan = m;
+            }
+        });
+
+        const key = {
+            id: 'any_key',
+            segment: 'any_segment'
+        };
+        const value = 'value';
+        const _ttl = 9999;
+
+        const delay = 50;
+        let start;
+        trojan.delay(delay + 1);
+
+        start = Date.now();
+        await client.start();
+        expect(Date.now() - start).to.least(delay);
+
+        start = Date.now();
+        await client.get(key);
+        expect(Date.now() - start).to.least(delay);
+
+        start = Date.now();
+        await client.set(key, value, _ttl);
+        expect(Date.now() - start).to.least(delay);
+
+        start = Date.now();
+        await client.drop(key);
+        expect(Date.now() - start).to.least(delay);
+
+        start = Date.now();
+        await client.stop();
+        expect(Date.now() - start).to.least(delay);
+
     });
 });
